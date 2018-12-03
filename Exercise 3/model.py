32dd5de28abfa8e86fad653fc7639115033f858c
Shashank Agnihotri <shashanksagnihotri@gmail.com>
2018-12-03 07:36:02 +0100

    Better than before

--- Exercise 3/model.py
+++ Exercise 3/model.py
@@ -3,114 +3,95 @@ import numpy as np
 
 class Model:
     
-    def __init__(self, history_length, lr = 0.0002, batch_size = 64):
+    def __init__(self, history_length, lr = 0.001, batch_size = 64):
         
         # TODO: Define network
         # ...
         '''SA'''
         #num_filters = 15
         #filter_size = 5
-
-        self.x_image = tf.placeholder(tf.float32, shape=[None,96,96,history_length], name = "x_image")
-        self.y_ = tf.placeholder(tf.float32, shape=[None, 4], name = "y_")
+        with tf.name_scope("inputs"):
+            self.x_image = tf.placeholder(tf.float32, shape=[None,96,96], name = "x_image")
+            reshape_x = tf.reshape(self.x_image, shape = [-1, 96, 96, history_length], name = "reshape_x")
+            self.y_ = tf.placeholder(tf.float32, shape=[None, 4], name = "y_")
         #y_conv = tf.placeholder(tf.float32, shape=[None, 10], name= 'y_conv')
 
-        '''
-
-        print("x_image:", self.x_image.shape)
-        print("y_:", self.y_.shape)
-
-        self.W_conv1 = self.weight_variable([filter_size, filter_size, 1, num_filters])
-        print("W_conv1:", self.W_conv1.shape)
-        self.b_conv1 = self.bias_variable([num_filters])
-        print("b_conv1:", self.b_conv1.shape)
-
-        self.h_conv1 = tf.nn.relu(self.conv2d(self.x_image, self.W_conv1) + self.b_conv1 , name = "h_conv1")
-        self.h_pool1 = self.max_pool_2x2(self.h_conv1)
-
-        self.W_conv2 = self.weight_variable([filter_size, filter_size, num_filters, num_filters])
-        self.b_conv2 = self.bias_variable([num_filters])
-
-        print("h_conv1:", self.h_conv1.shape)
-        print("h_pool1:", self.h_pool1.shape)
-        print("W_conv2:", self.W_conv2.shape)
-        print("b_conv2:", self.b_conv2.shape)
-
-
-        self.h_conv2 = tf.nn.relu(self.conv2d(self.h_pool1, self.W_conv2) + self.b_conv2 , name = "h_conv2")
-        self.h_pool2 = self.max_pool_2x2(self.h_conv2)
-        self.W_fc1 = self.weight_variable([96 * 96 * num_filters, 4])
-        self.b_fc1 = self.bias_variable([4])
-
-        print("h_conv2:", self.h_conv2.shape)
-        print("h_pool2:", self.h_pool2.shape)
-        print("W_fc1:", self.W_fc1.shape)
-        print("b_fc1:", self.b_fc1.shape)
-
-
-        self.h_pool2_flat = tf.reshape(self.h_pool2, [-1, 96*96*num_filters] , name = "h_pool2")
-
-        self.dense = tf.layers.dense(inputs = self.h_pool2_flat, units = 4, activation = tf.nn.relu , name = "dense")
-
-        self.y_pred = tf.layers.dense(inputs = self.dense, units = 4, name = "y_pred")
-
-        print("h_pool2flat:", self.h_pool2_flat.shape)
-        print("y_pred:", self.y_pred.shape)
-
-        
-        
-        self.h_fc1 = tf.nn.relu(tf.matmul(self.h_pool2_flat, self.W_fc1) + self.b_fc1 , name = "h_fc1")
-        self.keep_prob = tf.placeholder(tf.float32, name = "keep")
-        #keep_prob = 0.5
-        self.h_fc1_drop = tf.nn.dropout(self.h_fc1, self.keep_prob , name = "h_f1_drop")
-
-        self.W_fc2 = self.weight_variable([4, 4])
-        self.b_fc2 = self.bias_variable([4])
-
-        print("h_fc1:", self.h_fc1.shape)
-        print("h_fc1_drop:", self.h_fc1_drop.shape)
-
-        print("W_fc2:", self.W_fc2.shape)
-        print("b_fc2:", self.b_fc2.shape)
-
-        self.y_conv=tf.nn.softmax(tf.matmul(self.h_fc1_drop, self.W_fc2) + self.b_fc2 , name = "y_conv")
-
-        
-        
-        #self.y_conv=tf.nn.softmax(self.W_fc2 + self.b_fc2 , name = "y_conv")
-        print("y_conv:", self.y_conv.shape)
-
-        
-        exit()
-        self.n_samples = self.x_train.shape[0]
-        self.n_batches = self.n_samples // batch_size
-        '''
-
-        #batch_size = tf.shape(self.x_image)[0]
         # first layers
         self.W_conv1 = tf.get_variable("W_conv1", [8, 8, history_length, 64], initializer=tf.contrib.layers.xavier_initializer())
-        conv1 = tf.nn.conv2d(self.x_image, self.W_conv1, strides=[1, 2, 2, 1], padding='VALID')
+        conv1 = tf.nn.conv2d(reshape_x, self.W_conv1, strides=[1, 2, 2, 1], padding='VALID')
         conv1_a = tf.nn.relu(conv1)
+        pool1 = self.max_pool_2x2(conv1_a)
 
         # second layer
         self.W_conv2 = tf.get_variable("W_conv2", [4, 4, 64, 64], initializer=tf.contrib.layers.xavier_initializer())
-        conv2 = tf.nn.conv2d(conv1_a, self.W_conv2, strides=[1, 2, 2, 1], padding='VALID')
+        conv2 = tf.nn.conv2d(pool1, self.W_conv2, strides=[1, 2, 2, 1], padding='VALID')
         conv2_a = tf.nn.sigmoid(conv2)
+        pool2 = self.max_pool_2x2(conv2_a)
 
         # third layer
         self.W_conv3 = tf.get_variable("W_conv3", [3, 3, 64, 64], initializer=tf.contrib.layers.xavier_initializer())
-        conv3 = tf.nn.conv2d(conv2_a, self.W_conv3, strides=[1, 2, 2, 1], padding='VALID')
+        conv3 = tf.nn.conv2d(pool2, self.W_conv3, strides=[1, 2, 2, 1], padding='VALID')
         conv3_a = tf.nn.relu(conv3)
+        pool3 = self.max_pool_2x2(conv3_a)
 
+        
+        '''
         # fourth layer
-        self.W_conv4 = tf.get_variable("W_conv4", [4, 4, 64, 64], initializer=tf.contrib.layers.xavier_initializer())
-        conv4 = tf.nn.conv2d(conv3_a, self.W_conv4, strides=[1, 2, 2, 1], padding='VALID')
+        self.W_conv4 = tf.get_variable("W_conv4", [2, 2, 64, 64], initializer=tf.contrib.layers.xavier_initializer())
+        conv4 = tf.nn.conv2d(pool3, self.W_conv4, strides=[1, 2, 2, 1], padding='VALID')
         conv4_a = tf.nn.sigmoid(conv4)
+        pool4 = self.max_pool_2x2(conv4_a)
 
         # fifth layer
         self.W_conv5 = tf.get_variable("W_conv5", [4, 4, 64, 64], initializer=tf.contrib.layers.xavier_initializer())
-        conv5 = tf.nn.conv2d(conv4_a, self.W_conv5, strides=[1, 2, 2, 1], padding='VALID')
+        conv5 = tf.nn.conv2d(pool4, self.W_conv5, strides=[1, 2, 2, 1], padding='VALID')
         conv5_a = tf.nn.relu(conv5)
+        pool5 = self.max_pool_2x2(conv5_a)
+        '''
+        
+
+        flatten = tf.reshape(pool3, shape = [-1, 1*8*8])
+
+        fc1 = tf.contrib.layers.fully_connected(flatten, 1024, activation_fn=tf.nn.relu)
+        fc2 = tf.layers.dense(fc1, 1024, activation = tf.nn.sigmoid, name = "fc2")
+        
+
+        with tf.name_scope("output"):
+            self.logits = tf.layers.dense(fc2, 4, name = "output")
+            y_pred = tf.nn.softmax(self.logits, name = "y_pred")
+            self.predict = tf.argmax(self.logits, 1)
+            #print("\n\nPREDICTION:", self.predict)
+
+            
+
+        # TODO: Loss and optimizer
+        with tf.name_scope("train"):
+            cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.y_)
+            self.loss = tf.reduce_mean(cross_entropy)
+            self.optimizer = tf.train.GradientDescentOptimizer(lr).minimize(self.loss)
+            
+        with tf.name_scope("eval"):
+            self.prediction = tf.equal(tf.argmax(self.logits, 1), tf.argmax(self.y_, 1))
+            #prediction = tf.argmax(self.logits, 1)
+            self.accuracy = tf.reduce_mean(tf.cast(self.prediction, tf.float32))
+
+            
+
+            
+        # TODO: Start tensorflow session
+        with tf.name_scope("init_and_save"):
+            init = tf.global_variables_initializer()
+        
+        self.sess = tf.Session();
+        self.sess.run(init)
+        self.saver = tf.train.Saver()
+
+        
+
+
+
+        '''
+        #batch_size = tf.shape(self.x_image)[0]
 
 ##        # sixth layer
 ##        self.W_conv6 = tf.get_variable("W_conv6", [4, 4, 64, 64], initializer=tf.contrib.layers.xavier_initializer())
@@ -153,64 +134,29 @@ class Model:
 
         
         #cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
-        '''
+        
         self.cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels = self.y_ , logits = self.cost)
         self.cross_entropy = tf.reduce_mean(self.cross_entropy)*100
         self.train_step = tf.train.GradientDescentOptimizer(lr).minimize(self.cross_entropy)
         self.correct_prediction = tf.equal(tf.argmax(self.y_conv,1), tf.argmax(self.y_,1))
-        '''
+        
 
-        '''
+        
         self.prediction = tf.argmax(self.y_conv,1, name='prediction')
         self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32), name='accuracy')
-        '''
+        
 
         self.optimizer = tf.train.GradientDescentOptimizer(lr).minimize(self.cost)
 
-        #self.sess = tf.Session()
-        self.sess = tf.InteractiveSession()
+        self.sess = tf.Session()
+        #self.sess = tf.InteractiveSession()
         self.init = tf.global_variables_initializer()
 
         self.saver = tf.train.Saver()
-
-
-
-        '''
-        num_epochs = n_minibatches
-        learning_curve = np.zeros(num_epochs)
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name="accuracy")
-
-
-        # TODO: Start tensorflow session
-        # ...
-
-        with tf.Session() as sess:
-            #accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name="accuracy")
-            #sess.run(tf.initialize_all_variables())
-            sess.run(tf.global_variables_initializer())
-            
-            #learning_curve = np.zeros(num_epochs)
-            for i in range(num_epochs):
-                for b in range(n_batches):
-                    x_batch = x_train[b*batch_size:(b+1)*batch_size]
-                    y_batch = y_train[b*batch_size:(b+1)*batch_size]
-                    train_step.run(feed_dict={x_image: x_batch, y_: y_batch, keep_prob: 1.0})
-                    #train_step.run(feed_dict={x_image: x_batch, y_: y_batch})
-                        
-                    #print("step %d, training accuracy %g"%(i, train_accuracy))
-                #train_step.run(feed_dict={x_image: x_batch, y_: y_batch, keep_prob: 0.2})
-                #train_accuracy = 1 - accuracy.eval(feed_dict={x_image:x_train , y_: y_train})
-                    
-                learning_curve[i] = 1 - accuracy.eval(feed_dict={x_image: x_valid, y_: y_valid, keep_prob: 1.0})
-                #learning_curve[i] = 1 - accuracy.eval(feed_dict={x_image: x_valid, y_: y_valid})
-                print("step %d, train error %g"%(i, learning_curve[i]))
-            model_dir = saver.save(sess, os.path.join(model_dir, "agent.ckpt"))
-        #print('Model saved in file:')
-        print(model_dir)
         
-        self.saver = tf.train.Saver()
-
         '''
+    
+    
 
     def load(self, file_name):
         self.saver.restore(self.sess, file_name)
